8a849c370e055dad6aa003c7708700884783a96b
StyleHubOfficial <147305874+StyleHubOfficial@users.noreply.github.com>
2025-11-20 18:23:08 +0530

    Rename index.js to index.jsx

--- index.jsx
+++ index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react-dom/client';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
@@ -9,7 +9,7 @@ if (!rootElement) {
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-    React.createElement(React.StrictMode, null,
-        React.createElement(App, null)
-    )
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
